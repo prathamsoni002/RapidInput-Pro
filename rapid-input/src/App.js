33ceba5bc4b6c1987c02d70b3527ae230bc8380c
Pratham Soni <pratham.soni25@gmail.com>
2024-05-06 14:51:44 +0100

    feat: Implemented header component and associated styles. Added background color to the entire page and fixed header position at the top.

--- rapid-input/src/App.js
+++ rapid-input/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+// Import other components as needed
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Render the Header component */}
+      <Header />
+      {/* Render other components and UI elements */}
+      {/* For example: */}
+      {/* <FeatureMenu /> */}
+      {/* <TextDisplay /> */}
+      {/* <Timer /> */}
+      {/* <RestartButton /> */}
+      {/* Add more components as needed */}
     </div>
   );
 }
---
+++ rapid-input/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css'; 
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/logo1-nobg.png" alt="Logo" className="logo" />
+      <span className="app-name">RapidInput Pro</span>
+    </div>
+  );
+}
+
+export default Header;
